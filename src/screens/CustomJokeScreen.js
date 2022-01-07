a1c52ae5d41d841e62b6fbae04facca9e1e8bb0c
gldrobinson <gldrobinson@hotmail.co.uk>
2022-01-07 18:06:03 +0000

    sets up Custom Joke inputs with an alert

--- src/screens/CustomJokeScreen.js
+++ src/screens/CustomJokeScreen.js
@@ -1,10 +1,61 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { getCustomJoke } from "../api/apiRequest";
+import showJokeAlert from "../components/alert";
+import { setCustomJoke, setFirstName, setLastName } from "../state/actions";
 
 const CustomJokeScreen = () => {
+  const { customJoke, firstName, lastName } = useSelector(
+    (state) => state.custom
+  );
+
+  const dispatch = useDispatch();
+
+  const onSearch = () => {
+    getCustomJoke(firstName, lastName)
+      .then((joke) => {
+        dispatch(setCustomJoke(joke));
+      })
+      .then(() => {
+        showJokeAlert("Custom Joke", customJoke);
+      })
+      .catch((err) => {
+        const errorMessage = "Oops, something went wrong. Please try again!";
+        showJokeAlert("Error", errorMessage);
+      });
+  };
+
+  const handleFirstNameInput = (input) => {
+    dispatch(setFirstName(input));
+  };
+
+  const handleLastNameInput = (input) => {
+    dispatch(setLastName(input));
+  };
+
   return (
     <View style={styles.container}>
       <Text>Custom Joke Page</Text>
+      <View style={styles.textInputContainer}>
+        <View style={styles.textInput}>
+          <Text style={styles.textHint}>Enter first name: </Text>
+          <TextInput style={styles.input} onChangeText={handleFirstNameInput} />
+        </View>
+        <View style={styles.textInput}>
+          <Text style={styles.textHint}>Enter last name: </Text>
+          <TextInput style={styles.input} onChangeText={handleLastNameInput} />
+        </View>
+        <TouchableOpacity style={styles.button} onPress={onSearch}>
+          <Text style={styles.buttonText}>SEARCH</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -12,6 +63,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+  textInputContainer: {
+    flex: 1,
+    paddingTop: 100,
+    margin: 50,
+    justifyContent: "flex-start",
+  },
+  textHint: {
+    margin: 12,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "#4682b4",
+    width: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 2,
+    borderRadius: 10,
+    margin: 10,
+    color: "white",
+    fontWeight: "bold",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
 
