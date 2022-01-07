74fa101414d239b35af9a1a41594fb7bc6767875
gldrobinson <gldrobinson@hotmail.co.uk>
2022-01-07 14:53:37 +0000

    adds button and styling to HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { withSafeAreaInsets } from "react-native-safe-area-context";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Jokes list Page</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>RANDOM JOKE</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>TEXT INPUT</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>NEVER ENDING JOKES</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -12,6 +23,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "column",
+    paddingTop: 100,
+    alignItems: "center",
+    margin: 50,
+  },
+  button: {
+    padding: 20,
+    backgroundColor: "#4682b4",
+    width: 250,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 2,
+    borderRadius: 10,
+    margin: 10,
+    color: "white",
+    fontWeight: "bold",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
 
