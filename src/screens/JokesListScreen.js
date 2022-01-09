5c86c3be200ba2461ed8a1b2ebfc983bc4573edf
gldrobinson <gldrobinson@hotmail.co.uk>
2022-01-09 13:24:58 +0000

    adds flatList to screen with renderLoading function to display loading whilst fetching more jokes

--- src/screens/JokesListScreen.js
+++ src/screens/JokesListScreen.js
@@ -1,5 +1,11 @@
-import React, { useEffect } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { getMultipleJokes } from "../api/apiRequest";
 import { useSelector, useDispatch } from "react-redux";
 import { setMultipleJokes } from "../state/actions";
@@ -9,6 +15,10 @@ const JokesListScreen = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    requestMultipleJokes();
+  }, []);
+
+  const requestMultipleJokes = () => {
     getMultipleJokes()
       .then((jokes) => {
         dispatch(setMultipleJokes(jokes));
@@ -16,16 +26,30 @@ const JokesListScreen = () => {
       .catch((err) => {
         console.dir(err);
       });
-  }, []);
+  };
 
-  console.log(multipleJokes);
+  const renderLoading = () => {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator size="large" color="blue" />
+        <Text style={styles.textLoading}>Fetching more jokes!</Text>
+      </View>
+    );
+  };
 
   return (
     <View style={styles.container}>
       <Text>Jokes list Page</Text>
       <FlatList
+        style={styles.flatList}
         data={multipleJokes}
-        renderItem={({ item }) => <Text>{item.joke}</Text>}
+        renderItem={({ item }) => (
+          <Text style={styles.listText}>{item.id + "-->" + item.joke}</Text>
+        )}
+        onEndReachedThreshold={0.5}
+        onEndReached={requestMultipleJokes}
+        ListFooterComponent={renderLoading}
+        keyExtractor={(item, index) => index.toString()}
       />
     </View>
   );
@@ -34,6 +58,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+  flatList: {
+    padding: 10,
+  },
+  listText: {
+    fontSize: 18,
+    padding: 10,
+  },
+  textLoading: {
+    textAlign: "center",
+  },
+  loading: {
+    padding: 20,
   },
 });
 
